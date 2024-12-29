d55fe242c0ac16b91e19b3b7460eb179a4712487
Md. Najmul Hasan <najmul.csebuet@gmail.com>
2024-12-29 12:40:04 +0600

    Add a simple template for competitive programming in C++

---
+++ ultra_slim_template.cpp
@@ -0,0 +1,27 @@
+// simple template for competitive programming
+
+#include <bits/stdc++.h>
+using namespace std;
+
+using vi = vector<int>;
+using vs = vector<string>;
+
+#define sz(x) ((int)(x).size())
+#define rep(i, a, b) for (int i = a; i < b; i++)
+
+void solution() {}
+
+int main() {
+  int t = 1;
+#ifdef LOCAL
+  freopen("in.txt", "r", stdin);
+  freopen("out.txt", "w", stdout);
+  cin >> t;
+#endif
+
+  while (t--) {
+    solution();
+  }
+
+  return 0;
+}
